a8602f8bc38f6f9622c383890d3b90ba8e1f4919
d4Nc0D31902 <danielrodri1902@gmail.com>
2024-11-03 15:50:57 +0800

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,9 @@ const Portfolio = () => {
   const [volume, setVolume] = useState(0.2);
   const audioRef = useRef(null);
   const [isNotSmallScreen, setIsNotSmallScreen] = useState(
-    window.innerWidth > 768
+    window.innerWidth > 1024
   );
+
   const toggleAudio = () => {
     if (isMuted) {
       audioRef.current.play();
@@ -68,36 +69,38 @@ const Portfolio = () => {
 
   useEffect(() => {
     const handleResize = () => {
-      setIsNotSmallScreen(window.innerWidth > 768);
+      setIsNotSmallScreen(window.innerWidth > 1024);
     };
 
     window.addEventListener("resize", handleResize);
-    // Cleanup the event listener on component unmount
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
     <div
-      className={`h-screen ${
+      className={`desktop:h-screen tablets:h-screen phones:h-screen iphone:h-full ${
         isDarkTheme
           ? "bg-gradient-to-r from-black to-gray-800"
           : "bg-gradient-to-r from-white to-gray-600"
       } p-5`}
     >
       <audio ref={audioRef} src="../mp3/bgmusic.mp3" autoPlay loop />
-      <div className="grid md:grid-cols-4 md:grid-rows-4 p-2 h-[100%] gap-2 sm:grid-cols-2 sm:grid-rows-3 xs:grid-cols-1 xs:grid-rows-2">
+      <div
+        className="grid desktop:grid-cols-4 desktop:grid-rows-4 tablets:grid-cols-3 tablets:grid-rows-2 phones:grid-cols-2 phones:grid-rows-3 iphone:grid-cols-1 iphone:grid-rows-1 p-2 h-[100%] gap-2 
+      "
+      >
         {/* Theme Toggle Button */}
 
         {/* Profile Pic */}
         <div
-          className={`bg-white md:col-span-1 md:row-span-4 rounded-lg flex justify-center items-center shadow-2xl sm:col-span-1 sm:row-span-2 relative group neon-border ${
+          className={`bg-white desktop:col-span-1 desktop:row-span-4 rounded-lg flex justify-center items-center shadow-2xl tablets:col-span-1 tablets:row-span-2 phones:col-span-1 phones:row-span-2 iphone:col-span-1 iphone:row-span-1 relative group neon-border  ${
             isDarkTheme ? "bg-black" : "bg-white"
           }`}
         >
           {/* Theme Toggle Button */}
           <button
             onClick={toggleTheme}
-            className={`absolute top-5 right-5 p-2 rounded-full shadow-md transition-opacity duration-300 opacity-0 group-hover:opacity-100 ${
+            className={`absolute top-5 right-5 p-2 rounded-full shadow-lg transition-opacity duration-300 opacity-0 group-hover:opacity-100 ${
               isDarkTheme ? "bg-gray-300 text-black" : "bg-black text-white"
             }`}
           >
@@ -114,7 +117,7 @@ const Portfolio = () => {
             {/* Speaker/Mute Button */}
             <button
               onClick={toggleAudio}
-              className={`p-2 rounded-full shadow-md hover:bg-gray-200 ${
+              className={`p-2 rounded-full shadow-lg hover:bg-gray-200 ${
                 isDarkTheme ? "bg-gray-300" : "bg-white"
               }`}
             >
@@ -138,39 +141,45 @@ const Portfolio = () => {
 
         {/* Description */}
         <div
-          className={`md:col-span-2 md:row-span-2 rounded-lg p-4 flex flex-col justify-center items-start shadow-lg space-y-4 ${
+          className={`desktop:col-span-2 desktop:row-span-2 rounded-lg p-4 flex flex-col justify-center items-start shadow-lg space-y-4 ${
             isDarkTheme ? "bg-black text-white" : "bg-white text-black"
           }`}
         >
           <div className="w-full h-full">
-            <h4 className="text-2xl font-bold">
-              Hi👋 I'm Daniel Angelo O. Rodriguez,
-            </h4>
-
             {isNotSmallScreen ? (
-              <Typing
-                text={[
-                  "a passionate developer with a love for coding. I thrive on tackling new challenges and continuously expanding my skills in the ever-evolving tech landscape. With a curious mind and a strong desire to learn, I'm always exploring innovative solutions and embracing the latest technologies. Whether I'm building applications or collaborating with others, I aim to make a positive impact through my work.",
-                ]}
-                speed={40}
-                eraseSpeed={10}
-                eraseDelay={2000}
-                typingDelay={500}
-              />
+              <>
+                <h4 className="text-2xl font-bold">
+                  Hi 👋 I'm Daniel Angelo O. Rodriguez,
+                </h4>
+                <Typing
+                  text={[
+                    "a passionate developer with a love for coding. I thrive on tackling new challenges and continuously expanding my skills in the ever-evolving tech landscape. With a curious mind and a strong desire to learn, I'm always exploring innovative solutions and embracing the latest technologies. Whether I'm building applications or collaborating with others, I aim to make a positive impact through my work.",
+                  ]}
+                  speed={40}
+                  eraseSpeed={10}
+                  eraseDelay={2000}
+                  typingDelay={500}
+                />
+              </>
             ) : (
-              <p
-                className={`mt-2  ${
-                  isDarkTheme ? "text-gray-300" : "text-gray-700"
-                } text-md`}
-              >
-                a passionate developer with a love for coding. I thrive on
-                tackling new challenges and continuously expanding my skills in
-                the ever-evolving tech landscape.
-              </p>
+              <>
+                <h4 className="text-md font-bold">
+                  Hi 👋 I'm Daniel Angelo O. Rodriguez,
+                </h4>
+                <p
+                  className={`mt-2 ${
+                    isDarkTheme ? "text-gray-300" : "text-gray-700"
+                  } text-md`}
+                >
+                  a passionate developer with a love for coding. I thrive on
+                  tackling new challenges and continuously expanding my skills
+                  in the ever-evolving tech landscape.
+                </p>
+              </>
             )}
           </div>
 
-          <div className="w-full h-[20%] flex flex-col sm:flex-row items-center justify-center gap-4">
+          <div className="w-full h-[20%] flex flex-col md:flex-row items-center justify-center gap-4">
             {isNotSmallScreen ? (
               <div className="flex justify-between w-full">
                 <Tooltip title="Contact Me">
@@ -207,7 +216,7 @@ const Portfolio = () => {
                 </Tooltip>
               </div>
             ) : (
-              <>
+              <div className="flex justify-between w-full mb-2">
                 <Button
                   type="primary"
                   icon={<MailOutlined />}
@@ -219,7 +228,7 @@ const Portfolio = () => {
                       ? "bg-gray-700 text-white"
                       : "bg-white text-black"
                   }`}
-                  size="large"
+                  size="medium"
                 />
 
                 <Button
@@ -234,16 +243,16 @@ const Portfolio = () => {
                     backgroundColor: isDarkTheme ? "#4a5568" : "#52c41a",
                     borderColor: isDarkTheme ? "#4a5568" : "#52c41a",
                   }}
-                  size="large"
+                  size="medium"
                 />
-              </>
+              </div>
             )}
           </div>
         </div>
 
         {/* Socials */}
         <div
-          className={`shadow-lg md:col-span-1 md:row-span-2 rounded-lg p-2 flex justify-center flex-col gap-2 items-center ${
+          className={`shadow-lg desktop:col-span-1 desktop:row-span-2 rounded-lg p-2 flex justify-center flex-col gap-2 items-center ${
             isDarkTheme ? "bg-black" : "bg-white"
           }`}
         >
@@ -251,7 +260,7 @@ const Portfolio = () => {
             href="https://www.facebook.com/danielangelo.rodriguez.2002/"
             target="_blank"
             rel="noopener noreferrer"
-            className={`border-2 shadow-md rounded-lg w-[80%] h-[20%] flex gap-5 justify-center p-2 tems-center scale-hover ${
+            className={`border-2 shadow-lg rounded-lg w-[80%] h-[20%] flex gap-5 justify-center p-2 tems-center scale-hover ${
               isDarkTheme ? "bg-gray-700 text-white" : "bg-white text-black"
             }`}
           >
@@ -262,7 +271,7 @@ const Portfolio = () => {
             href="https://www.instagram.com/"
             target="_blank"
             rel="noopener noreferrer"
-            className={`border-2 shadow-md rounded-lg w-[80%] h-[20%] flex gap-5 justify-center p-2 tems-center scale-hover ${
+            className={`border-2 shadow-lg rounded-lg w-[80%] h-[20%] flex gap-5 justify-center p-2 tems-center scale-hover ${
               isDarkTheme ? "bg-gray-700 text-white" : "bg-white text-black"
             }`}
           >
@@ -273,7 +282,7 @@ const Portfolio = () => {
             href="https://github.com/d4Nc0D31902"
             target="_blank"
             rel="noopener noreferrer"
-            className={`border-2 shadow-md rounded-lg w-[80%] h-[20%] flex gap-5 justify-center p-2 tems-center 
+            className={`border-2 shadow-lg rounded-lg w-[80%] h-[20%] flex gap-5 justify-center p-2 tems-center 
               scale-hover ${
                 isDarkTheme ? "bg-gray-700 text-white" : "bg-white text-black"
               }`}
@@ -285,7 +294,7 @@ const Portfolio = () => {
             href="https://www.tiktok.com/@daniel.angelo.rod?is_from_webapp=1&sender_device=pc"
             target="_blank"
             rel="noopener noreferrer"
-            className={`border-2 shadow-md rounded-lg w-[80%] h-[20%] flex gap-5 justify-center p-2 tems-center
+            className={`border-2 shadow-lg rounded-lg w-[80%] h-[20%] flex gap-5 justify-center p-2 tems-center
               scale-hover ${
                 isDarkTheme ? "bg-gray-700 text-white" : "bg-white text-black"
               }`}
@@ -297,7 +306,7 @@ const Portfolio = () => {
 
         {/* Marquee */}
         <div
-          className={` shadow-lg md:col-span-1 md:row-span-2 rounded-lg p-2 flex justify-center items-center overflow-hidden relative ${
+          className={` shadow-lg desktop:col-span-1 desktop:row-span-2 rounded-lg p-2 flex justify-center items-center overflow-hidden relative ${
             isDarkTheme ? "bg-black" : "bg-white"
           }`}
         >
@@ -306,7 +315,7 @@ const Portfolio = () => {
               {items.map((item, index) => (
                 <div
                   key={index}
-                  className={`p-2 w-[250px] rounded-lg flex items-center gap-10 border-2 shadow-md ${
+                  className={`p-2 w-[250px] rounded-lg flex items-center gap-10 border-2 shadow-lg ${
                     isDarkTheme
                       ? "bg-gray-800 text-white"
                       : "bg-white text-black"
@@ -317,7 +326,7 @@ const Portfolio = () => {
                     alt={item.alt}
                     className="h-10 w-10 mr-2"
                   />
-                  <p className="text-md">{item.text}</p>
+                  <p className="text-lg">{item.text}</p>
                 </div>
               ))}
             </div>
@@ -326,7 +335,7 @@ const Portfolio = () => {
 
         {/* Works */}
         <div
-          className={`md:col-span-2 md:row-span-2 rounded-lg p-4 flex justify-center items-center gap-4 flex-wrap overflow-auto ${
+          className={`desktop:col-span-2 desktop:row-span-2 rounded-lg p-4 flex justify-center items-center gap-4 flex-wrap overflow-auto  ${
             isDarkTheme ? "bg-black" : "bg-white"
           } shadow-lg hover:shadow-xl transition-shadow duration-300`}
         >
@@ -358,7 +367,7 @@ const Portfolio = () => {
                 <div
                   key={index}
                   style={{ cursor: "pointer" }}
-                  className="relative group overflow-hidden rounded-lg shadow-md transition-transform transform hover:scale-105"
+                  className="relative group overflow-hidden rounded-lg shadow-lg transition-transform transform hover:scale-105"
                 >
                   <a
                     href={links[index]}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+      screens: {
+      'iphone': '414px',
+      'phones': '640px',
+      'tablets': '768px',
+      'desktop': '1024px',
+    },
   },
   plugins: [],
 };
