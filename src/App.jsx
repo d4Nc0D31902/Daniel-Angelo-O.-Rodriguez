85ac973b56582843efacf3de163542b8a1a05afa
d4Nc0D31902 <danielrodri1902@gmail.com>
2024-11-03 09:37:28 +0800

    more feat

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { Button, Tooltip } from "antd";
+import {
+  MailOutlined,
+  DownloadOutlined,
+  SoundOutlined,
+  MutedOutlined,
+  SunOutlined,
+  MoonOutlined,
+} from "@ant-design/icons";
 import Typing from "react-typing-effect";
-import { useMediaQuery } from "react-responsive";
-import { Button } from "antd";
-import { MailOutlined, DownloadOutlined } from "@ant-design/icons";
 
-const App = () => {
-  const isNotSmallScreen = useMediaQuery({ minWidth: 768 });
+const Portfolio = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState(true);
+  const [isMuted, setIsMuted] = useState(false);
+  const [volume, setVolume] = useState(1);
+  const audioRef = useRef(null);
+  const isNotSmallScreen = window.innerWidth > 768;
+  const toggleAudio = () => {
+    if (isMuted) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+    setIsMuted(!isMuted);
+  };
+
+  const handleVolumeChange = (e) => {
+    const newVolume = e.target.value;
+    setVolume(newVolume);
+    audioRef.current.volume = newVolume;
+  };
+
+  const toggleTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+  };
+
   const items = [
     { src: "../images/mongodb.png", alt: "MongoDB logo", text: "MongoDB" },
-    {
-      src: "../images/express.png",
-      alt: "ExpressJS logo",
-      text: "ExpressJS",
-    },
+    { src: "../images/express.png", alt: "ExpressJS logo", text: "ExpressJS" },
     { src: "../images/react.png", alt: "ReactJS logo", text: "ReactJS" },
     { src: "../images/node.png", alt: "NodeJS logo", text: "NodeJS" },
     { src: "../images/php.png", alt: "PHP logo", text: "PHP" },
@@ -29,30 +54,76 @@ const App = () => {
       alt: "TypeScript logo",
       text: "TypeScript",
     },
-    {
-      src: "../images/vite.png",
-      alt: "Vite logo",
-      text: "Vite",
-    },
-    {
-      src: "../images/figma.png",
-      alt: "Figma logo",
-      text: "Figma",
-    },
+    { src: "../images/vite.png", alt: "Vite logo", text: "Vite" },
+    { src: "../images/figma.png", alt: "Figma logo", text: "Figma" },
   ];
+
   return (
-    <div className="h-screen bg-gradient-to-r from-black to-gray-600 p-5">
+    <div
+      className={`h-screen ${
+        isDarkTheme
+          ? "bg-gradient-to-r from-black to-gray-600"
+          : "bg-gradient-to-r from-white to-gray-300"
+      } p-5`}
+    >
+      <audio ref={audioRef} src="../mp3/bgmusic.mp3" autoPlay loop />
       <div className="grid md:grid-cols-4 md:grid-rows-4 p-2 h-[100%] gap-2 sm:grid-cols-2 sm:grid-rows-3">
+        {/* Theme Toggle Button */}
+
         {/* Profile Pic */}
-        <div className="bg-white md:col-span-1 md:row-span-4 rounded-lg flex justify-center items-center shadow-2xl sm:col-span-1 sm:row-span-2">
+        <div
+          className={`bg-white md:col-span-1 md:row-span-4 rounded-lg flex justify-center items-center shadow-2xl sm:col-span-1 sm:row-span-2 relative group neon-border ${
+            isDarkTheme ? "bg-gray-800" : "bg-white"
+          }`}
+        >
+          {/* Theme Toggle Button */}
+          <button
+            onClick={toggleTheme}
+            className={`absolute top-5 right-5 p-2 rounded-full shadow-md transition-opacity duration-300 opacity-0 group-hover:opacity-100 ${
+              isDarkTheme ? "bg-gray-300 text-black" : "bg-black text-white"
+            }`}
+          >
+            {isDarkTheme ? <SunOutlined /> : <MoonOutlined />}
+          </button>
+
           <img
             src="../images/dan1.jpg"
             alt="Portfolio"
             className="w-full h-full object-cover rounded-lg"
           />
+          {/* Container for Button and Slider */}
+          <div className="absolute bottom-4 left-0 right-0 flex items-center gap-2 px-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            {/* Speaker/Mute Button */}
+            <button
+              onClick={toggleAudio}
+              className={`p-2 rounded-full shadow-md hover:bg-gray-200 ${
+                isDarkTheme ? "bg-gray-300" : "bg-white"
+              }`}
+            >
+              {isMuted ? <MutedOutlined /> : <SoundOutlined />}
+            </button>
+
+            {/* Volume Slider */}
+            <input
+              type="range"
+              min="0"
+              max="1"
+              step="0.01"
+              value={volume}
+              onChange={handleVolumeChange}
+              className={`w-full h-1 rounded-full cursor-pointer ${
+                isDarkTheme ? "bg-gray-600" : "bg-gray-300"
+              }`}
+            />
+          </div>
         </div>
+
         {/* Description */}
-        <div className="bg-white md:col-span-2 md:row-span-2 rounded-lg p-4 flex flex-col justify-center items-start shadow-lg space-y-4">
+        <div
+          className={`md:col-span-2 md:row-span-2 rounded-lg p-4 flex flex-col justify-center items-start shadow-lg space-y-4 ${
+            isDarkTheme ? "bg-gray-700 text-white" : "bg-white text-black"
+          }`}
+        >
           <div className="w-full h-full">
             <h4 className="text-2xl font-bold">
               Hi👋 I'm Daniel Angelo O. Rodriguez,
@@ -69,7 +140,11 @@ const App = () => {
                 typingDelay={500}
               />
             ) : (
-              <p className="mt-2 text-gray-700 text-xs">
+              <p
+                className={`mt-2 ${
+                  isDarkTheme ? "text-gray-300" : "text-gray-700"
+                } text-xs`}
+              >
                 a passionate developer with a love for coding. I thrive on
                 tackling new challenges and continuously expanding my skills in
                 the ever-evolving tech landscape.
@@ -79,29 +154,34 @@ const App = () => {
 
           <div className="w-full h-[20%] flex flex-col sm:flex-row items-center justify-center gap-4">
             {isNotSmallScreen ? (
-              <>
-                <Button
-                  type="primary"
-                  icon={<MailOutlined />}
-                  href="https://mail.google.com/mail/?view=cm&fs=1&to=danielrodri1902@gmail.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex items-center gap-2 text-sm sm:text-base px-4 py-2"
-                >
-                  Contact Me
-                </Button>
+              <div className="flex justify-between w-full">
+                <Tooltip title="Contact Me">
+                  <Button
+                    type="primary"
+                    icon={<MailOutlined />}
+                    href="https://mail.google.com/mail/?view=cm&fs=1&to=danielrodri1902@gmail.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center justify-center rounded-full p-2"
+                    size="large"
+                  />
+                </Tooltip>
 
-                <Button
-                  type="primary"
-                  icon={<DownloadOutlined />}
-                  href="../images/Resume.pdf"
-                  download
-                  className="flex items-center gap-2 text-sm sm:text-base px-4 py-2"
-                  style={{ backgroundColor: "#52c41a", borderColor: "#52c41a" }}
-                >
-                  Download Resume
-                </Button>
-              </>
+                <Tooltip title="Download Resume">
+                  <Button
+                    type="primary"
+                    icon={<DownloadOutlined />}
+                    href="../images/Resume.pdf"
+                    download
+                    className="flex items-center justify-center rounded-full p-2"
+                    style={{
+                      backgroundColor: "#52c41a",
+                      borderColor: "#52c41a",
+                    }}
+                    size="large"
+                  />
+                </Tooltip>
+              </div>
             ) : (
               <>
                 <Button
@@ -129,21 +209,29 @@ const App = () => {
         </div>
 
         {/* Socials */}
-        <div className="bg-white shadow-lg md:col-span-1 md:row-span-2 rounded-lg p-2 flex justify-center flex-col gap-2 items-center">
+        <div
+          className={`shadow-lg md:col-span-1 md:row-span-2 rounded-lg p-2 flex justify-center flex-col gap-2 items-center ${
+            isDarkTheme ? "bg-gray-800" : "bg-white"
+          }`}
+        >
           <a
             href="https://www.facebook.com/danielangelo.rodriguez.2002/"
             target="_blank"
             rel="noopener noreferrer"
-            className="border-2 shadow-md rounded-lg w-[80%] h-[20%] flex gap-5 justify-center p-2 tems-center"
+            className={`border-2 shadow-md rounded-lg w-[80%] h-[20%] flex gap-5 justify-center p-2 tems-center ${
+              isDarkTheme ? "bg-gray-700 text-white" : "bg-white text-black"
+            }`}
           >
             <img src="../images/facebook.png" />
             <p className="flex items-center">Facebook</p>
           </a>
           <a
-            href="https://www.facebook.com/danielangelo.rodriguez.2002/"
+            href="https://www.instagram.com/"
             target="_blank"
             rel="noopener noreferrer"
-            className="border-2 shadow-md rounded-lg w-[80%] h-[20%] flex gap-5 justify-center p-2 tems-center"
+            className={`border-2 shadow-md rounded-lg w-[80%] h-[20%] flex gap-5 justify-center p-2 tems-center ${
+              isDarkTheme ? "bg-gray-700 text-white" : "bg-white text-black"
+            }`}
           >
             <img src="../images/insta.png" />
             <p className="flex items-center">Instagram</p>
@@ -152,7 +240,9 @@ const App = () => {
             href="https://github.com/d4Nc0D31902"
             target="_blank"
             rel="noopener noreferrer"
-            className="border-2 shadow-md rounded-lg w-[80%] h-[20%] flex gap-8 justify-center p-2 tems-center"
+            className={`border-2 shadow-md rounded-lg w-[80%] h-[20%] flex gap-5 justify-center p-2 tems-center ${
+              isDarkTheme ? "bg-gray-700 text-white" : "bg-white text-black"
+            }`}
           >
             <img src="../images/github.png" />
             <p className="flex items-center">Github</p>
@@ -161,7 +251,9 @@ const App = () => {
             href="https://www.tiktok.com/@daniel.angelo.rod?is_from_webapp=1&sender_device=pc"
             target="_blank"
             rel="noopener noreferrer"
-            className="border-2 shadow-md rounded-lg w-[80%] h-[20%] flex gap-8 justify-center p-2 tems-center"
+            className={`border-2 shadow-md rounded-lg w-[80%] h-[20%] flex gap-5 justify-center p-2 tems-center ${
+              isDarkTheme ? "bg-gray-700 text-white" : "bg-white text-black"
+            }`}
           >
             <img src="../images/tik-tok.png" />
             <p className="flex items-center">Tiktok</p>
@@ -169,13 +261,21 @@ const App = () => {
         </div>
 
         {/* Marquee */}
-        <div className="bg-white shadow-lg md:col-span-1 md:row-span-2 rounded-lg p-2 flex justify-center items-center overflow-hidden relative">
+        <div
+          className={`bg-white shadow-lg md:col-span-1 md:row-span-2 rounded-lg p-2 flex justify-center items-center overflow-hidden relative ${
+            isDarkTheme ? "bg-gray-800" : "bg-white"
+          }`}
+        >
           <div className="marquee gap-y-5">
             <div className="flex flex-col space-y-5">
               {items.map((item, index) => (
                 <div
                   key={index}
-                  className="p-2 w-[250px] rounded-lg flex items-center gap-10 border-2 shadow-md"
+                  className={`p-2 w-[250px] rounded-lg flex items-center gap-10 border-2 shadow-md ${
+                    isDarkTheme
+                      ? "bg-gray-700 text-white"
+                      : "bg-white text-black"
+                  }`}
                 >
                   <img
                     src={item.src}
@@ -190,7 +290,11 @@ const App = () => {
         </div>
 
         {/* Works */}
-        <div className="md:col-span-2 md:row-span-2 rounded-lg p-4 flex justify-center items-center gap-4 flex-wrap overflow-auto bg-white shadow-lg hover:shadow-xl transition-shadow duration-300">
+        <div
+          className={`md:col-span-2 md:row-span-2 rounded-lg p-4 flex justify-center items-center gap-4 flex-wrap overflow-auto ${
+            isDarkTheme ? "bg-gray-800" : "bg-white"
+          } shadow-lg hover:shadow-xl transition-shadow duration-300`}
+        >
           {Array(4)
             .fill()
             .map((_, index) => {
@@ -232,7 +336,11 @@ const App = () => {
                       alt={imageNames[index]}
                       className="object-cover rounded-lg w-64 h-32 shadow-2 border-2"
                     />
-                    <div className="absolute inset-0 bg-black bg-opacity-30 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                    <div
+                      className={`absolute inset-0 bg-black bg-opacity-30 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 ${
+                        isDarkTheme ? "bg-opacity-70" : "bg-opacity-30"
+                      }`}
+                    >
                       <span className="text-white text-lg font-semibold text-wrap text-center">
                         {imageNames[index]}{" "}
                       </span>
@@ -247,4 +355,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Portfolio;
